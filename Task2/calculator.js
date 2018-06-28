5d65e0ad256665b1c2196f8676877ebc050f7caf
AbizharTaris <abizhar.kiryuji@gmail.com>
2018-06-28 23:51:25 +0700

    Berisi operasi-operasi perhitungan kalkulator

---
+++ Task2/calculator.js
@@ -0,0 +1,27 @@
+function tambahAB (a,b) {
+    return a+b;
+}
+
+function kurangAB (a,b) {
+    return a-b;
+}
+
+function bagiAB (a,b) {
+    return a/b;
+}
+
+function kaliAB (a,b) {
+    return a*b;
+}
+
+function akarAB (a,b) {
+    return Math.sqrt(a,b);
+}
+
+module.exports = {
+    tambahAB,
+    kurangAB,
+    bagiAB,
+    kaliAB,
+    akarAB
+}
