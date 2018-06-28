24144670506c9604e9e2ad25e866079805e2f208
AbizharTaris <abizhar.kiryuji@gmail.com>
2018-06-28 23:54:34 +0700

    File untuk menampilkan hasil perhitungan kalkulator

---
+++ Task2/main.js
@@ -0,0 +1,9 @@
+var operations = require("./calculator.js");
+
+let a = 6;
+let b = 2;
+console.log(operations.tambahAB(a,b));
+console.log(operations.kurangAB(a,b));
+console.log(operations.kaliAB(a,b));
+console.log(operations.bagiAB(a,b));
+console.log(operations.akarAB(a,b));
